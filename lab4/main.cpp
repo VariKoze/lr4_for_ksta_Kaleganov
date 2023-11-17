dfbb0a2fab53173a9e1a97e6ff025ec4bbc6b2a4
kaleganov andrey <andreykaleganov50@gmail.com>
2023-11-18 01:31:25 +0300

    add test  function in main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -5,11 +5,26 @@
 
 using namespace std;
 
+bool test()
+{
+    string res_test_1 = censor("dog eat monkey");
+    string res_test_2 = censor("kiwi eat zebra");
+    string res_test_3 = censor("rabbit eat food");
+    string res_test_4 = censor("food eat food, ok and?!");
+    string res_test_5 = censor("Monkey flip");
+    if ((res_test_1 == "*** eat ***") && (res_test_2 == "*** eat ***") && (res_test_3 == "*** eat ***") &&
+        (res_test_4 == "*** eat ***, ok and?!")  && (res_test_5 == "*** flip"))
+    {
+        return true;
+    }
+    else return false;
+}
+
 int main()
 {
-    string in;
-    cout << "Write something:" << endl;
-    getline(cin,in);
-    cout<<censor(in);
-    return 0;
+    if (test() == true)
+    {
+        cout << "Test complete";
+    }
+    else cout << "Test failed";
 }
